406cc4759f7f92f5b6088761a9c69546b837e732
justaninstrument <kwabenaasamoahadjei@gmail.com>
2022-06-15 01:29:02 +0000

    new code for sorting

--- dope.py
+++ dope.py
@@ -1,2 +1,17 @@
-print ("Hello World")
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+if a > b:
+    a = a + b
+    b = a - b
+    a = a - b
+c = float(input("Enter c: "))
+if b > c:
+    b = b + c
+    c = b - c
+    b = b - c
+    if a > b:
+        a = a + b
+        b = a - b
+        a = a - b
+print (a, "<", b, "<", c)
 
